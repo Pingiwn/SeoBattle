6ba05a2845738714aa8abce762a9575774c531cf
Pingiwn <75725871+Pingiwn@users.noreply.github.com>
2022-02-09 22:20:19 +0300

    Create version 1.0

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -2,12 +2,24 @@ public class SimpleDotCom {
     int[] locationCells;
     int numOfHits = 0;
 
-    public String checkYouself(String userGuess) {
-
-        return userGuess;
+    public String checkYouself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "мимо";
+        for(int cell: locationCells){
+            if (guess == cell){
+                result = "Попал";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length){
+            result = "Потопил";
+        }
+        System.out.println(result);
+        return  result;
     }
-    public void setLocationCells(int[] locationCells){
-        System.out.println(locationCells[0]);
+    public void setLocationCells(int[] locs){
+        locationCells  = locs;
     }
 
 }
--- src/SimpleDotComeGame.java
+++ src/SimpleDotComeGame.java
@@ -1,5 +1,9 @@
 public class SimpleDotComeGame {
     public static void main(String[] args) {
-
+        SimpleDotCom dot = new SimpleDotCom();
+        int[] location = { 2, 3, 4 };
+        dot.setLocationCells(location);
+        String userGuess = "2";
+        String string = dot.checkYouself(userGuess);
     }
 }
