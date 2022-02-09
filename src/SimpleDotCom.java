949b1ed588e173024fa4becc8b29e0eefa117245
Pingiwn <75725871+Pingiwn@users.noreply.github.com>
2022-02-09 21:45:59 +0300

    First commit

---
+++ src/SimpleDotCom.java
@@ -0,0 +1,13 @@
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    public String checkYouself(String userGuess) {
+
+        return userGuess;
+    }
+    public void setLocationCells(int[] locationCells){
+        System.out.println(locationCells[0]);
+    }
+
+}
---
+++ src/SimpleDotComeGame.java
@@ -0,0 +1,5 @@
+public class SimpleDotComeGame {
+    public static void main(String[] args) {
+
+    }
+}
